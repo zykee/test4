9c440dab808c006c137de00d542ff01f41bbd9d3
zykee <3349567853@qq.com>
2017-06-23 20:35:32 +0800

    test4

---
+++ main/person.js
@@ -0,0 +1,2 @@
+// Write your code here
+module.exports = Person
---
+++ main/student.js
@@ -0,0 +1,14 @@
+let Person = require('./person')
+// Write your code here
+module.exports = Student
+function Student(name,age,klass)
+{
+	person.call(this,name,age)
+	tihs.klass=klass
+	function introduce()
+	{
+		alert("My name is"+ this.name". I am"+this.age"years old."+". I am at Class "+this.klass+".")
+	}
+
+}
+worker.prototype=new Person() 
