af5359b5cf4c2243623cf5ef825d03994abb6605
zykee <3349567853@qq.com>
2017-06-23 20:46:11 +0800

    just for test4.1

--- main/person.js
+++ main/person.js
@@ -1,2 +1,11 @@
 // Write your code here
 module.exports = Person
+function Person(name,age)
+{
+	this.name="Tom"
+	this.age=18
+}
+Person.prototype.introduce=function()
+{
+	alert("My name is"+ this.name". I am"+this.age"years old.")	
+}
--- main/student.js
+++ main/student.js
@@ -5,10 +5,9 @@ function Student(name,age,klass)
 {
 	person.call(this,name,age)
 	tihs.klass=klass
-	function introduce()
-	{
-		alert("My name is"+ this.name". I am"+this.age"years old."+". I am at Class "+this.klass+".")
-	}
-
 }
-worker.prototype=new Person() 
+Student.prototype=new Person() 
+Student.prototype.showklass()
+{
+	alert(". I am at Class "+this.klass+".")
+}
